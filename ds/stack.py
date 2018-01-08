7bf669107f9ff552b07f4021c643bae92c6321c8
Samuel <samthan@umich.edu>
2018-01-07 17:52:17 -0800

    added to stack impl with fixed size array

--- ds/stack.py
+++ ds/stack.py
@@ -1,3 +1,5 @@
+from array import *
+
 # stack implementation with list...kind of cheating
 # stack supports 4 basic operations and is Last In First Out
 class Stack:
@@ -25,12 +27,75 @@ class Stack:
 		for i in reversed(self.stack):
 			print i
 
-s = Stack()
 
-print s.empty()
+class ArrayStack:
 
-for i in range(10):
-	s.add(i)
+	def __init__(self):
+		self.arr = [None] * 5
+		self.capacity = 5
+		self.size = 0
+		self.top = self.arr[self.size]
+
+	def add(self, data):
+		if self.size == self.capacity:
+			# print "expanding"
+			self.capacity *= 2
+			resize = [None] * self.capacity
+
+			for i in range(self.size):
+				resize[i] = self.arr[i]
+
+			resize[self.size] = data
+
+			self.size += 1
+			self.arr = resize
+		else:
+			# print "within capacity"
+			self.arr[self.size] = data
+			self.size += 1
+
+		self.top = self.arr[self.size - 1]
+
+	def remove(self):
+		self.arr[self.size - 1] = None
+		self.size -= 1
+		self.top = self.arr[self.size - 1]
+
+	def empty(self):
+
+		return self.size == 0
+
+	def peek(self):
+		return self.top
+
+	def show(self):
+		for i in reversed(self.arr):
+			if i is not None:
+				print i
+
+
+a = ArrayStack()
+for i in range(6):
+	a.add(i)
+
+# print a.peek()
+a.show()
+print "-------"
+a.remove()
+print a.peek()
+a.show()
+# s = Stack()
 
 for i in range(5):
-	s.remove()
+	a.remove()
+
+print a.peek()
+print a.empty()
+
+# print s.empty()
+
+# for i in range(10):
+# 	s.add(i)
+
+# for i in range(5):
+# 	s.remove()
